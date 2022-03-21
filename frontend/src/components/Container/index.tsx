345e8b0eff2a99eb206ec99ba6fd9cd1d8afbc2e
Milton Carlos Katoo <mckatoo@gmail.com>
2022-03-21 15:48:10 -0300

    Bordas arredontadas

--- frontend/src/components/Container/index.tsx
+++ frontend/src/components/Container/index.tsx
@@ -6,7 +6,7 @@ const Container = ({ children }) => (
       <TopMenu />
     </div>
     <section>
-      <div id="main" className="main-content flex-1 bg-gray-100 mt-12 md:mt-2 pb-24 md:pb-5">
+      <div id="main" className="main-content rounded-tl-md flex-1 bg-gray-100 mt-12 md:mt-2 pb-24 md:pb-5">
         {children}
       </div>
     </section>
