8ae01ce629ca5a5752541d54126374996d8de882
Milton Carlos Katoo <mckatoo@gmail.com>
2022-03-26 15:34:14 -0300

    Move top menu from container to index

--- frontend/src/components/Container/index.tsx
+++ frontend/src/components/Container/index.tsx
@@ -1,16 +1,11 @@
 import TopMenu from "../TopMenu"
 
 const Container = ({ children }) => (
-  <div className="grid-row-2 w-full">
-    <div className="text-right">
-      <TopMenu />
+  <section>
+    <div id="main" className="main-content rounded-tl-md flex-1 bg-gray-100 mt-12 md:mt-2 pb-24 md:pb-5">
+      {children}
     </div>
-    <section>
-      <div id="main" className="main-content rounded-tl-md flex-1 bg-gray-100 mt-12 md:mt-2 pb-24 md:pb-5">
-        {children}
-      </div>
-    </section>
-  </div>
+  </section>
 )
 
 export default Container
